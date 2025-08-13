3adc61f683450c6986315e0e23c5a3ff770ee195
Sara <saraashraf55530@gmail.com>
2025-08-13 21:06:22 +0300

    Initialize project

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:news/home_screen.dart';
+
+void main() {
+  runApp(NewsApp());
+}
+
+class NewsApp extends StatelessWidget {
+  const NewsApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      routes: {HomeScreen.routeName: (_) => HomeScreen()},
+      initialRoute: HomeScreen.routeName,
+    );
+  }
+}
