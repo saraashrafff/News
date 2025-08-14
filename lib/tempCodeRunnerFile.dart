4d7dc1e9ba5a95aab0372d905fafba54943f3524
Sara <saraashraf55530@gmail.com>
2025-08-14 21:31:19 +0300

    Add splash screen

---
+++ lib/tempCodeRunnerFile.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:news/app_theme.dart';
+import 'package:news/home_screen.dart';
+
+void main() {
+  runApp(NewsApp());
+}
+
+class NewsApp extends StatelessWidget {
+  const NewsApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      routes: {HomeScreen.routeName: (_) => HomeScreen()},
+      initialRoute: HomeScreen.routeName,
+      theme: AppTheme.lightTheme,
+      darkTheme: AppTheme.darkTheme,
+      themeMode: ThemeMode.light,
+    );
+  }
+}
