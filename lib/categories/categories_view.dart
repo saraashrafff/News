c5a87045560420974189917f2a42a5ffeee1bc0e
Sara <saraashraf55530@gmail.com>
2025-08-14 16:16:02 +0300

    Implement home drawer

--- lib/categories/categories_view.dart
+++ lib/categories/categories_view.dart
@@ -3,8 +3,8 @@ import 'package:news/categories/category_item.dart';
 import 'package:news/models/category_model.dart';
 
 class CategoriesView extends StatelessWidget {
-  const CategoriesView({super.key});
-
+  void Function(CategoryModel) onCatergorySelected;
+  CategoriesView({required this.onCatergorySelected});
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -19,8 +19,12 @@ class CategoriesView extends StatelessWidget {
           Expanded(
             child: ListView.separated(
               padding: EdgeInsets.only(top: 16),
-              itemBuilder: (_, index) =>
-                  CategoryItem(CategoryModel.categories[index]),
+              itemBuilder: (_, index) => GestureDetector(
+                onTap: () {
+                  onCatergorySelected(CategoryModel.categories[index]);
+                },
+                child: CategoryItem(CategoryModel.categories[index]),
+              ),
               separatorBuilder: (_, _) => SizedBox(height: 16),
               itemCount: CategoryModel.categories.length,
             ),
---
+++ lib/drawer/home_drawer.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:news/app_theme.dart';
+
+class HomeDrawer extends StatelessWidget {
+  VoidCallback onGoToHomeClicked;
+  HomeDrawer({required this.onGoToHomeClicked});
+  @override
+  Widget build(BuildContext context) {
+    TextTheme textTheme = Theme.of(context).textTheme;
+    Size screenSize = MediaQuery.sizeOf(context);
+    return Container(
+      color: AppTheme.black,
+      width: screenSize.width * 0.7,
+      child: Column(
+        children: [
+          Container(
+            alignment: Alignment.center,
+            height: screenSize.height * 0.2,
+            width: double.infinity,
+            color: AppTheme.white,
+            child: Text(
+              'News App',
+              style: textTheme.titleLarge!.copyWith(
+                color: AppTheme.black,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.all(16),
+            child: GestureDetector(
+              onTap: () {
+                onGoToHomeClicked();
+                Navigator.pop(context);
+              },
+              child: Row(
+                children: [
+                  SvgPicture.asset('assets/icons/home.svg'),
+                  SizedBox(width: 8),
+                  Text('Go To Home', style: textTheme.labelLarge),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
